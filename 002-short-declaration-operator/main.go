527ef48c14c0e09e8360da98f20353b8441e58c1
Jason Liu <jasonliu1990@gmail.com>
2021-01-24 21:42:35 +0800

    Create main.go

---
+++ 002-short-declaration-operator/main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	x := 42 // declare and assign variables
+	fmt.Println(x)
+	x = 99 // already declared, so use the equal sign to assign a new value
+	fmt.Println(x)
+	y := 100 + 24
+	fmt.Println(y)
+}
