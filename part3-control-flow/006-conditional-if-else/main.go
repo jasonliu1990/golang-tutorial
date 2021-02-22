8a9ae4d76ec94f68b79bed9fb7de1e2bc5416d78
Jason Liu <jasonliu1990@gmail.com>
2021-02-22 23:36:54 +0800

    Create main.go

---
+++ part3-control-flow/006-conditional-if-else/main.go
@@ -0,0 +1,15 @@
+package main
+
+func main(){
+	x := 42
+	if x == 40{
+		println("x = 40")
+	} else if x == 42{
+		println("x = 42")
+	} else if x == 42{
+		println("x = 42")
+	} else{
+		print("x != 40, 41, 42")
+	}
+
+}
