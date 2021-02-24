47024f108266dc7d2b52251cfe2a599ede45a43d
Jason Liu <jasonliu1990@gmail.com>
2021-02-24 23:29:08 +0800

    Create main.go

---
+++ part3-control-flow/009-conditional-logic-operators/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main()  {
+	fmt.Printf("true && true: %v\n", true && true)
+	fmt.Printf("true && false: %v\n", true && false)
+	fmt.Printf("true || false: %v\n", true || false)
+	fmt.Printf("true || true: %v\n", true || true)
+	fmt.Printf("!true: %v \n", !true)
+}
